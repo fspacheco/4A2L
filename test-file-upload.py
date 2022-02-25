4028ce4c535c1441f062091c87d59fb9eeb27760
Fernando S. Pacheco <fspacheco@gmail.com>
2022-02-25 14:40:25 -0300

    Shows assessment msg (only one)

--- test-file-upload.py
+++ test-file-upload.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # https://blog.miguelgrinberg.com/post/handling-file-uploads-with-flask
 
 import os
@@ -7,6 +10,8 @@ from werkzeug.utils import secure_filename
 from logging.config import dictConfig
 
 from parseaia import Project
+
+import emoji
 
 dictConfig({
     'version': 1,
@@ -25,13 +30,21 @@ dictConfig({
 })
     
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 1000 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1000 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.aia']
 app.config['UPLOAD_PATH'] = 'uploads'
+
+@app.template_filter('emojify')
+def emoji_filter(s):
+    return emoji.emojize(s)
 
 @app.route('/')
 def index():
     return render_template('file-upload.html')
+
+@app.errorhandler(413)
+def too_large(e):
+    return "File is too large", 413
 
 @app.route('/', methods=['POST'])
 def upload_file():
@@ -40,12 +53,13 @@ def upload_file():
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-            abort(400)        
+            return "Invalid file format", 400        
         app.logger.info('%s submitted succesfully', filename)
         save_filename = os.path.join(app.config['UPLOAD_PATH'], filename)
         uploaded_file.save(save_filename)
         app.logger.debug('%s saved', save_filename)
-        parse_aia_file(save_filename)
+        output_parser=parse_aia_file(save_filename)
+        return render_template('output.html', msg=output_parser)
     return redirect(url_for('index'))
 
 def parse_aia_file(filename):
@@ -55,6 +69,8 @@ def parse_aia_file(filename):
     # Check number of screens
     if (len(mp.screens) == 1):
         app.logger.info("OK  : Tem uma tela")
+        return(':check_mark_button:    Tem uma tela')
     else:
         app.logger.info("FAIL: Número de telas não é um")
+        return(':cross_mark:    Número de telas não é um :red_exclamation_mark:')
 
